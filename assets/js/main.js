245c41b59e11e6fa5e4e1c5ed1b1dba830182141
DwiPashaDjango <dwipasha336@gmail.com>
2023-07-18 11:33:05 +0700

    bukuCuy

---
+++ assets/js/main.js
@@ -0,0 +1,253 @@
+function myFunction() {
+    var elms = document.getElementsByClassName("menu");
+    Array.from(elms).forEach((x) => {
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    })
+}
+
+function chart(element) {
+    document.getElementById('booksModal').style.display = 'block';
+    let image = element.getAttribute("data-id");
+    var imageContainer = document.getElementById("append");
+    var imageElement = document.createElement("img");
+    imageElement.src = image; 
+
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(imageElement);
+}
+
+$(document).ready(function () {
+    
+    $('.banner-area').slick({
+        autoplay: true,
+        speed: 100,
+        arrows: false,
+        dots: true,
+        fade: true
+    });
+
+    $('#login').click(function (e) {
+        e.preventDefault();
+        $('#loginModal').css('display', 'block');
+    });
+
+    $('.close').click(function (e) {
+        e.preventDefault();
+        $('#booksModal').css('display', 'none');
+    });
+
+    $('#register').click(function (e) {
+        e.preventDefault();
+        $('#registerModal').css('display', 'block');
+    });
+
+    $('.close').click(function (e) {
+        e.preventDefault();
+        $('#loginModal').css('display', 'none');
+    });
+
+    $('.close').click(function (e) {
+        e.preventDefault();
+        $('#registerModal').css('display', 'none');
+    });
+
+    $('#link-login').click(function (e) {
+        e.preventDefault();
+        $('#registerModal').css('display', 'none');
+        $('#loginModal').css('display', 'block');
+    });
+
+    $('#link-register').click(function (e) {
+        e.preventDefault();
+        $('#loginModal').css('display', 'none');
+        $('#registerModal').css('display', 'block');
+    });
+
+    $('#save').click(function (e) {
+        e.preventDefault();
+        let name = $('#name').val();
+        let nim = $('#nim').val();
+        let email = $('#email').val();
+
+        if (name == '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Isikan nama saudara!',
+            })
+        } else if (nim == '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Isikan nim saudara!',
+            })
+        } else if (email == '') {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Isikan email saudara!',
+            })
+        } else {
+            let timerInterval
+            Swal.fire({
+                icon: 'success',
+                title: 'Meminjam Buku.',
+                html: 'Prosessing in <b></b> milliseconds.',
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval);
+                    $('#booksModal').css('display', 'none');
+                    $('.form-validation')[0].reset();
+                }
+            }).then((result) => {
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    console.log('I was closed by the timer');
+                    $('#booksModal').css('display', 'none');
+                    $('.form-validation')[0].reset();
+                }
+            })
+        }
+    });
+
+    $('#post-login').click(function (e) {
+        e.preventDefault();
+        let username = $('#username').val();
+        let password = $('#password').val();
+
+        if (username == '') {
+            $('#msg').fadeIn();
+            $('#msg').html(`<div class="alert-dangers">
+                                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                                Masukan Username.
+                            </div>`);
+            setTimeout(() => {
+                $('#msg').fadeOut();
+            }, 2000);
+        } else if (password == '') {
+            $('#msg').fadeIn();
+            $('#msg').html(`<div class="alert-dangers">
+                                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                                Masukan Password.
+                            </div>`);
+            setTimeout(() => {
+                $('#msg').fadeOut();
+            }, 2000);
+        } else {
+            let timerInterval
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil.',
+                html: 'Prosessing in <b></b> milliseconds.',
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                    b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval);
+                    $('#loginModal').css('display', 'none');
+                    window.location.href = '../user/dashboard.html';
+                }
+            }).then((result) => {
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    console.log('I was closed by the timer');
+                    $('#loginModal').css('display', 'none');
+                    window.location.href = '../user/dashboard.html';
+                }
+            })
+        }
+    });
+
+    $('#post-register').click(function (e) {
+        e.preventDefault();
+        let username = $('#username_regis').val();
+        let email = $('#email_regis').val();
+        let password = $('#password_regis').val();
+        let password_confirmation = $('#password_confirmation_regis').val();
+
+        if (username == '') {
+            $('#err').fadeIn();
+            $('#err').html(`<div class="alert-dangers">
+                                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                                Userame Tidak Boleh Kosong.
+                            </div>`);
+            setTimeout(() => {
+                $('#err').fadeOut();
+            }, 2000);
+        } else if (email == '') {
+            $('#err').fadeIn();
+            $('#err').html(`<div class="alert-dangers">
+                                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                                Email Tidak Boleh Kosong.
+                            </div>`);
+            setTimeout(() => {
+                $('#err').fadeOut();
+            }, 2000);
+        } else if (password == '') {
+            $('#err').fadeIn();
+            $('#err').html(`<div class="alert-dangers">
+                                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                                Password Tidak Boleh Kosong.
+                            </div>`);
+            setTimeout(() => {
+                $('#err').fadeOut();
+            }, 2000);
+        } else if (password_confirmation == '') {
+            $('#err').fadeIn();
+            $('#err').html(`<div class="alert-dangers">
+                                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                                Konfirmasi Password Tidak Boleh Kosong.
+                            </div>`);
+            setTimeout(() => {
+                $('#err').fadeOut();
+            }, 2000);
+        } else {
+            $('#registerModal').css('display', 'none');
+            $('#loginModal').css('display', 'block');
+
+            $('#msg').fadeIn();
+            $('#msg').html(`<div class="alert alert-success">
+                                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+                                <strong>Berhasil!</strong> silahkan login.
+                            </div>`);
+            setTimeout(() => {
+                $('#msg').fadeOut();
+            }, 2000);
+        }
+    });
+
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery:{
+			enabled: true,
+			navigateByImgClick: true,
+			tPrev: 'Previous (Left arrow key)',
+			tNext: 'Next (Right arrow key)',
+			tCounter: '%curr% of %total%',
+			preload: [0,1]
+		},
+		image:{
+			tError: '<a href="%url%">The Image #%curr%</a> Tidak Dapat Diproses',
+			titleSrc: function(item){
+			}
+		}
+    });
+});
---
+++ assets/js/style.js
@@ -0,0 +1,124 @@
+$(document).ready(function () {
+    var xBar = ["2020", "2021", "2022", "2023"];
+    var barColors = ["red", "green", "blue", "orange", "brown"];
+    var yBar = [81, 200, 60, 250];
+
+    const barChart = new Chart('barChart', {
+        type: 'bar',
+        data: {
+            labels: xBar,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yBar
+            }]
+        },
+        options: {
+            maintainAspecRatio: false,
+            layout: {
+                padding: {
+                    left: 10,
+                    rigth: 25,
+                    top: 25,
+                    bottom: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: 'month'
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: true
+                    },
+                    ticks: {
+                        maxTicksLimit: 6,
+                    },
+                    maxBarThickness: 50
+                }]
+            },
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 15000,
+                    maxTicksLimit: 5,
+                    padding: 10,
+                    callback: function(value, index, values) {
+                        return 'Rp.' + number_format(value);
+                    }
+                },
+                gridLines: {
+                    color: "rgba(234, 236, 244)",
+                    zeroLineColor: "rgba(234, 236, 244)",
+                    drawBorder: true,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                }
+            }]
+        },
+        legend: {
+            display: false
+        }
+
+    });
+
+    var xLine = ["2020", "2021", "2022", "2023"];;
+    var yLine = [100, 25, 351, 250];
+    const pieChart = new Chart('pieChart', {
+        type: 'line',
+        data: {
+            labels: xLine,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yLine
+            }]
+        },
+        options: {
+            maintainAspecRatio: false,
+            layout: {
+                padding: {
+                    left: 10,
+                    rigth: 25,
+                    top: 25,
+                    bottom: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: 'month'
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: true
+                    },
+                    ticks: {
+                        maxTicksLimit: 6,
+                    },
+                    maxBarThickness: 50
+                }]
+            },
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    max: 15000,
+                    maxTicksLimit: 5,
+                    padding: 10,
+                    callback: function (value, index, values) {
+                        return 'Rp.' + number_format(value);
+                    }
+                },
+                gridLines: {
+                    color: "rgba(234, 236, 244)",
+                    zeroLineColor: "rgba(234, 236, 244)",
+                    drawBorder: true,
+                    borderDash: [2],
+                    zeroLineBorderDash: [2]
+                }
+            }]
+        },
+        legend: {
+            display: true
+        }
+    });
+});
